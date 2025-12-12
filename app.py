99a0b88de25bba3ba4d3abc39681e44d672217fa
AndreasEdo <skult141000@gmail.com>
2025-12-12 07:23:45 +0700

    link

--- app.py
+++ app.py
@@ -83,6 +83,7 @@ class YOLOVideoTransformer(VideoTransformerBase):
 # ============================
 # PAGE LAYOUT
 # ============================
+st.markdown("<a href=\"https://swayo.vercel.app/categories.html\"> < Go Back ", unsafe_allow_html=True)
 st.markdown("<h1>🗑️ SWAYO: Smart Waste Classifier with YOLO</h1>", unsafe_allow_html=True)
 st.write("Deteksi sampah secara real-time menggunakan model YOLOv8 yang sudah dilatih khusus untuk klasifikasi sampah.")
 
